d30c4eb13a34abb4731d4439ad84c297855292c8
Filipe Magioni Mattos <filipe.mattos@conexos.inf.br>
2022-07-27 17:16:23 -0300

    Finalizado a customização do tailwind no projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./src/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '14': '3.5rem',
+        '125px': '125px'
+      },
+      colors: {
+        'hoverBtn': '#18D760',
+        'purpleMain': '#2D46B9',
+        'greenMain': '#1ED760'
+      },
+      backgroundImage: theme => ({
+        'spotify-theme': "url('./img/bursts.svg')",
+        'spotify-theme-mobile': "url('./img/bursts-mobile.svg')"
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%'
+      },
+      backgroundPosition: {
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center'
+      },
+      fontSize: {
+        '9xl': '9rem'
+      }
+    },
   },
   plugins: [],
 }
